2f5ff13c0c66b19a00d479a0df4b95efeede9f66
suomesta <shawn316michaels@gmail.com>
2025-07-27 14:02:01 +0900

    add three Integer toml

--- unittest/test_samples.cpp
+++ unittest/test_samples.cpp
@@ -343,6 +343,58 @@ TEST_CASE("String_8.toml") {
     CHECK(t["str"].get_string() == "'That,' she said, 'is still pointless.'");
 }
 
+TEST_CASE("Integer_1.toml") {
+    std::string content = load_file("Integer_1.toml");
+    item_t t(view_t{content.c_str()});
+
+    CHECK(t.is_table() == true);
+    CHECK(t.size() == 4);
+    CHECK(t["int1"].is_integer() == true);
+    CHECK(t["int1"].get_integer() == 99);
+    CHECK(t["int2"].is_integer() == true);
+    CHECK(t["int2"].get_integer() == 42);
+    CHECK(t["int3"].is_integer() == true);
+    CHECK(t["int3"].get_integer() == 0);
+    CHECK(t["int4"].is_integer() == true);
+    CHECK(t["int4"].get_integer() == -17);
+}
+
+TEST_CASE("Integer_2.toml") {
+    std::string content = load_file("Integer_2.toml");
+    item_t t(view_t{content.c_str()});
+
+    CHECK(t.is_table() == true);
+    CHECK(t.size() == 4);
+    CHECK(t["int5"].is_integer() == true);
+    CHECK(t["int5"].get_integer() == 1000);
+    CHECK(t["int6"].is_integer() == true);
+    CHECK(t["int6"].get_integer() == 5349221);
+    CHECK(t["int7"].is_integer() == true);
+    CHECK(t["int7"].get_integer() == 5349221);
+    CHECK(t["int8"].is_integer() == true);
+    CHECK(t["int8"].get_integer() == 12345);
+}
+
+TEST_CASE("Integer_3.toml") {
+    std::string content = load_file("Integer_3.toml");
+    item_t t(view_t{content.c_str()});
+
+    CHECK(t.is_table() == true);
+    CHECK(t.size() == 6);
+    CHECK(t["hex1"].is_integer() == true);
+    CHECK(t["hex1"].get_integer() == 3735928559);
+    CHECK(t["hex2"].is_integer() == true);
+    CHECK(t["hex2"].get_integer() == 3735928559);
+    CHECK(t["hex3"].is_integer() == true);
+    CHECK(t["hex3"].get_integer() == 3735928559);
+    CHECK(t["oct1"].is_integer() == true);
+    CHECK(t["oct1"].get_integer() == 342391);
+    CHECK(t["oct2"].is_integer() == true);
+    CHECK(t["oct2"].get_integer() == 493);
+    CHECK(t["bin1"].is_integer() == true);
+    CHECK(t["bin1"].get_integer() == 214);
+}
+
 TEST_CASE("Boolean_1.toml") {
     std::string content = load_file("Boolean_1.toml");
     item_t t(view_t{content.c_str()});
