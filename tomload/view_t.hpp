9eb29a4fe2171313c24de19e6d1b77b079317fd4
suomesta <shawn316michaels@gmail.com>
2025-08-06 02:32:50 +0900

    add comments to view_t.hpp

--- tomload/view_t.hpp
+++ tomload/view_t.hpp
@@ -1,3 +1,9 @@
+/*
+ * @file view_t.hpp
+ * @brief Header file for view_t class.
+ * @details This file defines the view_t class, which is a wrapper around std::string_view or string_view14,
+ *          providing utility functions for string manipulation.
+ */
 
 #ifndef TOMLOAD_VIEW_T_HPP
 #define TOMLOAD_VIEW_T_HPP
@@ -18,14 +24,25 @@ using view_t = std::string_view;
 using view_t = string_view14;
 #endif
 
+/* 
+ * @brief Same as std::string_view::starts_with() in C++ 20.
+ */
 inline bool starts_with(view_t view, view_t x) {
     return view.substr(0, x.size()) == x;
 }
+/////////////////////////////////////////////////////////////////////////////
 
+/* 
+ * @brief Same as std::string_view::ends_with() in C++ 20.
+ */
 inline bool ends_with(view_t view, view_t x) {
     return view.size() >= x.size() && view.compare(view.size() - x.size(), view_t::npos, x) == 0;
 }
+/////////////////////////////////////////////////////////////////////////////
 
+/* 
+ * @brief Util function like Python str.startswith([list]).
+ */
 inline bool starts_with(view_t view, std::initializer_list<const char*> list) {
     for (const char* str : list) {
         if (starts_with(view, str)) {
@@ -34,7 +51,11 @@ inline bool starts_with(view_t view, std::initializer_list<const char*> list) {
     }
     return false;
 }
+/////////////////////////////////////////////////////////////////////////////
 
+/* 
+ * @brief Util function like Python str.endswith([list]).
+ */
 inline bool ends_with(view_t view, std::initializer_list<const char*> list) {
     for (const char* str : list) {
         if (ends_with(view, str)) {
@@ -43,6 +64,7 @@ inline bool ends_with(view_t view, std::initializer_list<const char*> list) {
     }
     return false;
 }
+/////////////////////////////////////////////////////////////////////////////
 
 }  // namespace tomload
 
