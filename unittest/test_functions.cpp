4c925c50dc8dec8d9569884581424a36491d6c16
suomesta <shawn316michaels@gmail.com>
2025-08-23 16:08:47 +0900

    add one test to test_functions

--- unittest/test_functions.cpp
+++ unittest/test_functions.cpp
@@ -715,4 +715,10 @@ TEST_CASE("testing parse()") {
         CHECK(result["aa"]["bb"].is_boolean() == true);
         CHECK(result["aa"]["bb"].get_boolean() == false);
     }
+    {
+        view_t src = "[a.b.c.d]\n  z = 9\n\n[a]\n  b.c.d.k.t = \"Using dotted keys to add to [a.b.c.d] after explicitly defining it above is not allowed\"\n";
+
+        CHECK_THROWS_AS(parse_item(src), parse_error&);
+    }
 }
+
